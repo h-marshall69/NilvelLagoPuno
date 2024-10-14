078b050c5d9497b25dd7f4e2e9dea87be2168d13
h.marshall <h.jermaine.lee@gmail.com>
2024-10-13 19:53:46 -0700

    first commit

---
+++ Mensual.py
@@ -0,0 +1,26 @@
+import requests
+
+url = "https://www.senamhi.gob.pe/include/ajax-informacion-mensual.php"
+
+headers = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0',
+}
+
+# Carga útil (payload)
+data = {
+    'fecha': '2024-10',
+}
+
+# Enviar la solicitud POST
+response = requests.post(url, headers=headers, data=data)
+
+def filtrar_datos(data):
+    p = next((entry for entry in data['content'] if entry['nomEsta'] == 'MUELLE ENAFER'), None)
+    return p
+
+
+#print(filtrar_datos(response.json()))
+
+print(response.json())
